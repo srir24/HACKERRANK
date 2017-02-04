acfb837551114f6527d31f683ec407762e7e6463
srir24 <ravula.srikanh@gmail.com>
2017-02-04 12:58:10 +0530

    Create Read_lines.java

---
+++ Read_lines.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+         int b = scan.nextInt();
+         int c = scan.nextInt();
+      //------------------ Alternat code - Begin ------------------//
+       int a;
+       while(sc.hasNext()){
+         a=sc.nextInt();          
+         System.out.println(a);
+      }
+     //------------------ Alternat code - End --------------------//
+        System.out.println(a+"\n"+b+"\n"+c);
+    }
+}
